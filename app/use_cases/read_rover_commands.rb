78f2c987df9cc7cae2f2b9ce4b365877668eefe3
Victor Giraldes <victorpgiraldes@gmail.com>
2024-12-22 09:40:16 -0300

    Improve use case

--- app/use_cases/read_rover_commands.rb
+++ app/use_cases/read_rover_commands.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class ReadRoverCommands
-  def initialize(path:, command_runner_class: RoverCommandRunner, plateau_class: Plateau)
+  def initialize(path:, command_runner_class: RoverCommandRunner, plateau_class: Plateau, file: File)
     @path = path
     @rovers = []
     @command_runner_class = command_runner_class
     @plateau_class = plateau_class
+    @file = file
   end
 
   def call
-    file[1..-1].each_slice(2) do |rover_position, rover_instructions|
+    rover_commands_file[1..-1].each_slice(2) do |rover_position, rover_instructions|
       x, y, direction = rover_position.split(' ')
       rover = Rover.new(x.to_i, y.to_i, direction, plateau)
 
@@ -18,16 +19,16 @@ class ReadRoverCommands
       @rovers << rover
     end
 
-    File.open('public/output.txt', 'w') do |file|
+    @file.open('public/output.txt', 'w') do |file|
       file.write(@rovers.map(&:position).join("\n"))
     end
   end
 
-  def file
-    @file ||= File.read(@path).strip.split("\n")
+  def rover_commands_file
+    @rover_commands_file ||= @file.read(@path).strip.split("\n")
   end
 
   def plateau
-    @plateau ||= @plateau_class.new(*file.first.split(' ').map(&:to_i))
+    @plateau ||= @plateau_class.new(*rover_commands_file.first.split(' ').map(&:to_i))
   end
 end
