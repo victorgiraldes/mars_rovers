4fa96e397caddb710a374388c217390fc4d9969f
Victor Giraldes <victorpgiraldes@gmail.com>
2024-12-21 22:34:03 -0300

    Adjust tests

---
+++ spec/services/read_rover_commands_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'ReadRoverCommands' do
+  let(:subject) { described_class.new(path: path) }
+
+  let(:path) { 'spec/fixtures/rover_commands.txt' }
+  let(:plateau) { build(:plateau) }
+
+  describe '#call' do
+    it 'should create rovers' do
+      expect(Rover).to receive(:new).with(1, 2, 'N', plateau).and_call_original
+      expect(Rover).to receive(:new).with(3, 3, 'E', plateau).and_call_original
+
+      subject.call
+    end
+  end
+end
